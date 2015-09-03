4d9f65b03bbf0a0050ae97beacd9bb76b0241c1e
Brian Haas <brianjohnhaas@gmail.com>
2015-09-03 07:43:20 -0400

    add

---
+++ testing/run_DOMI.sh
@@ -0,0 +1,1 @@
+../DISCASM --aligned_bam Aligned.sortedByCoord.out.bam --chimeric_junctions Chimeric.out.junction --left_fq reads_1.fq.gz --right_fq reads_2.fq.gz --denovo_assembler OasesMultiK --out_dir DOMI_outdir
